de9951f907091ef23100658d76ff64785a7b67ff
terribledevice <longlost@gmail.com>
2017-11-18 13:37:59 -0500

    initial outline of script

--- date_calc.py
+++ date_calc.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+
 '''
-simple date calculator
+simple date calculator that takes a user input for date and finds
+finds the difference either in days or another date. 
+
 '''
-from datetime import date
+from datetime import datetime
 
-# gets today's date, stores in a variable
+date_one = input("What's the first date? ")
+date_two = input("What's the second date? ")
 
-# prompts user if they want to add or subtract days from a date
-# or find the number of days between dates
+def days_between(d1,d2):
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(d2, "%Y-%m-%d")
+    print(abs((d2 - d1).days))
 
-# displays calculation
 
+days_between(date_one,date_two)
